be40fc5886ae398522abde45b38013b3aa8c6dbb
gontzalaparicio <laztnog@hotmail.com>
2014-03-06 16:50:40 +0100

    cambios2

---
+++ controlador.php
@@ -0,0 +1,31 @@
+<?php
+
+
+   $val1= $_POST["value1"];
+   $val2= $_POST["value2"];
+   $operador= $_POST["operator"];
+
+   switch ($operador) {
+       case "+":
+           res=(int)val1+(int)val2;
+           break;
+       case "-":
+           res=(int)val1-(int)val2;
+           break;
+       case "*":
+           res=(int)val1*(int)val2;
+           break;
+       case "/":
+           res=(int)val1/(int)val2;
+           break;
+       case "^":
+           res=(int)val1^(int)val2;
+           break;
+   }
+   $string=preg_replace('/<div id=\"myid\">.*<\/div>/',"<br>"+result,file_get_contents('vista.html'));
+
+   
+   echo $string;
+
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 echo file_get_contents('vista.html');
 
 
